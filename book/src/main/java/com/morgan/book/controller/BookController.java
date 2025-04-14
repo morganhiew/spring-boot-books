4deb33cd2612b732c4bc2fa862205d3392f33967
Morgan Hiew <40155018+morganhiew@users.noreply.github.com>
2025-04-15 00:01:02 +0800

    added book controller consumer producer json constraint. added health unit test

--- book/src/main/java/com/morgan/book/controller/BookController.java
+++ book/src/main/java/com/morgan/book/controller/BookController.java
@@ -3,11 +3,13 @@ package com.morgan.book.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,24 +17,25 @@ import com.morgan.book.model.Book;
 import com.morgan.book.service.BookService;
 
 @RestController
+@RequestMapping(path = "/books", produces = MediaType.APPLICATION_JSON_VALUE)
 public class BookController {
 
   @Autowired
   private BookService bookService;
 
-  @PostMapping("/books")
+  @PostMapping(path = "", consumes = MediaType.APPLICATION_JSON_VALUE)
   public Book postBook(@RequestBody Book book) {
     Book createdBook = bookService.create(book);
     return createdBook;
   }
 
-  @GetMapping("/books")
+  @GetMapping("")
   public List<Book> getBooks(@RequestParam(required = false) String author,
       @RequestParam(required = false) Boolean published) {
     return bookService.list(author, published);
   }
 
-  @DeleteMapping("/books/{id}")
+  @DeleteMapping("/{id}")
   public void deleteBook(@PathVariable Long id) {
     bookService.delete(id);
   }
---
+++ book/src/test/java/com/morgan/book/controller/HealthCheckControllerTest.java
@@ -0,0 +1,27 @@
+package com.morgan.book.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = HealthCheckController.class)
+@AutoConfigureMockMvc
+public class HealthCheckControllerTest {
+
+        @Autowired
+        private MockMvc mvc;
+
+        @Test
+        public void testHealthCheck() throws Exception {
+                ResultActions positiveCase = mvc.perform(get("/health"));
+                positiveCase.andExpect(status().isOk())
+                                .andExpect(content().string("Healthy!"));
+        }
+
+}
